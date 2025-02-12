c6290fd52651b707609d1dd4f27de3f5b6678f32
mdecarlo26 <marcadecarlo@gmail.com>
2025-02-12 13:14:37 -0500

    testing

--- src/main.c
+++ src/main.c
@@ -111,7 +111,7 @@ int main() {
 
     printf("Starting Sim\n");
     int num_chunks = TIME_WINDOW / TAU;
-    for (int d = 0; d < NUM_SAMPLES; d++) {
+    for (int d = 110; d < NUM_SAMPLES; d++) {
         int **firing_counts = (int **)malloc(network.layers[network.num_layers - 1].num_neurons * sizeof(int *));
         for (int i = 0; i < network.layers[network.num_layers - 1].num_neurons; i++) {
             firing_counts[i] = (int *)calloc(num_chunks, sizeof(int));
