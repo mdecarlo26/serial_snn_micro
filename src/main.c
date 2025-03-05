1a36a0c6a3f552b635baa7e8caed51927b681e1e
mdecarlo26 <marcadecarlo@gmail.com>
2025-03-05 01:50:15 -0500

    try again

--- src/main.c
+++ src/main.c
@@ -147,6 +147,7 @@ int main() {
         for (int chunk = 0; chunk < TIME_WINDOW; chunk += TAU) {
             int chunk_index = chunk / TAU;
             printf("Processing Chunk %d\n", chunk);
+            printf("%d\n", network.layers[0].num_neurons);
             // Initialize input spikes for the first layer from the loaded data
             for (int t = 0; t < TAU; t++) {
                 for (int i = 0; i < network.layers[0].num_neurons; i++) {
