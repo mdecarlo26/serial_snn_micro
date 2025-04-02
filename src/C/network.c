9411eb992b623c415d41f5f3cc5ad6fea127ccb9
mdecarlo26 <marcadecarlo@gmail.com>
2025-04-01 21:53:54 -0400

    2 class

--- src/C/network.c
+++ src/C/network.c
@@ -43,7 +43,7 @@ void update_layer(const char **input, char **output, Layer *layer, int input_siz
                 }
             }
                        
-            printf("Neuron %d: Old Membrane Potential = %f\n", i, layer->neurons[i].membrane_potential);
+            // printf("Neuron %d: Old Membrane Potential = %f\n", i, layer->neurons[i].membrane_potential);
 
             float new_mem = 0;
             int reset_signal = heaviside(layer->neurons[i].membrane_potential,layer->neurons[i].voltage_thresh);
@@ -51,7 +51,7 @@ void update_layer(const char **input, char **output, Layer *layer, int input_siz
             layer->neurons[i].membrane_potential = new_mem;
             int output_spike = heaviside(layer->neurons[i].membrane_potential, layer->neurons[i].voltage_thresh);
             set_bit(output, i, t, output_spike); // Reset output for this time step
-            printf("Neuron %d: Membrane Potential = %f, Output = %d, Reset: %d, Sum: %d\n", i, layer->neurons[i].membrane_potential, output_spike, reset_signal, sum);
+            // printf("Neuron %d: Membrane Potential = %f, Output = %d, Reset: %d, Sum: %d\n", i, layer->neurons[i].membrane_potential, output_spike, reset_signal, sum);
         }
     }
 }
