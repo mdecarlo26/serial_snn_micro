899a3591a69e9a3afbaf16511e131c1a8af3dd06
mdecarlo26 <marcadecarlo@gmail.com>
2025-04-11 16:46:02 -0400

    test

--- arduino_stuff/ard_code/ard_code.ino
+++ arduino_stuff/ard_code/ard_code.ino
@@ -4,14 +4,8 @@
 Snn_Network snn_network;
 
 // 3. Fully static memory for ping-pong buffers
-char *ping_pong_buffer_1_data[MAX_NEURONS];
-char *ping_pong_buffer_2_data[MAX_NEURONS];
-
-char **ping_pong_buffer_1 = ping_pong_buffer_1_data;
-char **ping_pong_buffer_2 = ping_pong_buffer_2_data;
-
-static char ping_pong_buffer_1_blocks[MAX_NEURONS][TAU] = {0};
-static char ping_pong_buffer_2_blocks[MAX_NEURONS][TAU] = {0};
+static uint8_t ping_pong_buffer_1[MAX_NEURONS][BITMASK_BYTES] = {0};
+static uint8_t ping_pong_buffer_2[MAX_NEURONS][BITMASK_BYTES] = {0};
 
 // 6. Fully static memory for labels
 char labels[NUM_SAMPLES] = {0};
@@ -25,9 +19,7 @@ float* weights_fc2[NUM_CLASSES];
 
 
 
-static char initial_spikes_data[NUM_SAMPLES][TIME_WINDOW][INPUT_SIZE] = {0};
-char* initial_spikes_pointers_2d[NUM_SAMPLES][TIME_WINDOW];
-char** initial_spikes[NUM_SAMPLES];
+static uint8_t initial_spikes[NUM_SAMPLES][TIME_WINDOW][INPUT_BYTES] = {0};
 
 
 void setup() {
@@ -37,10 +29,6 @@ void setup() {
     }
     snn_network.num_layers = NUM_LAYERS;
     int neurons_per_layer[] = {INPUT_SIZE, HIDDEN_LAYER_1, NUM_CLASSES};
-    for (int i = 0; i < MAX_NEURONS; i++) {
-        ping_pong_buffer_1_data[i] = ping_pong_buffer_1_blocks[i];
-        ping_pong_buffer_2_data[i] = ping_pong_buffer_2_blocks[i];
-    }
 
     for (int i = 0; i < HIDDEN_LAYER_1; i++) {
         weights_fc1[i] = weights_fc1_data[i];
@@ -51,12 +39,6 @@ void setup() {
 
     initialize_network(neurons_per_layer, weights_fc1, weights_fc2, bias_fc1, bias_fc2);
     zero_network();
-    for (int i = 0; i < NUM_SAMPLES; i++) {
-        for (int j = 0; j < TIME_WINDOW; j++) {
-            initial_spikes_pointers_2d[i][j] = initial_spikes_data[i][j];
-        }
-        initial_spikes[i] = initial_spikes_pointers_2d[i];
-    }
 
     labels[0] = label;
 
@@ -64,10 +46,11 @@ void setup() {
 }
 
 void loop() {
-    int classification = inference(initial_spikes[0], ping_pong_buffer_1, ping_pong_buffer_2);
+    int d = 0;
+    int classification = inference(initial_spikes, ping_pong_buffer_1, ping_pong_buffer_2, d);
     Serial.print("Sample 0: Classification = ");
     Serial.print(classification);
     Serial.print(", Label = ");
-    Serial.println(labels[0]);
+    Serial.println(labels[d]);
     delay(1000); // Delay for 1 second before the next iteration
 }
--- arduino_stuff/ard_code/define.h
+++ arduino_stuff/ard_code/define.h
@@ -2,8 +2,8 @@
 #define DEFINE_H
 
 #define MAX_LAYERS 10
-#define MAX_NEURONS 800
-#define TAU 2
+#define MAX_NEURONS 1000
+#define TAU 10
 #define VOLTAGE_THRESH 1.0
 #define DECAY_RATE 0.95
 #define NUM_SAMPLES 1 // Total dataset size
@@ -13,6 +13,8 @@
 #define HIDDEN_LAYER_1 256
 #define NUM_LAYERS 3
 
+#define BITMASK_BYTES ((TAU + 7) / 8)
+#define INPUT_BYTES ((INPUT_SIZE + 7) / 8)
 
 
 #endif // DEFINE_H
