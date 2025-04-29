6646814281e47f9fcc11e62279c7e224e3a39e51
mdecarlo26 <marcadecarlo@gmail.com>
2025-04-29 14:18:29 -0400

    fixed

--- src/C/define.h
+++ src/C/define.h
@@ -27,7 +27,7 @@
 #define INPUT_BYTES ((INPUT_SIZE + 7) / 8)
 
 // Define the quantization parameters for Q0.7
-#define Q07_FLAG       1
+#define Q07_FLAG       0
 #define Q07_SCALE      128.0f
 #define Q07_INV_SCALE  (1.0f / 128.0f)
 #define Q07_MAX_FLOAT  0.9921875f   // 127 / 128
