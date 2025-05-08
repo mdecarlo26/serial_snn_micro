85bcfe4f1f6560780cb769459638ed3382cca2cb
mdecarlo26 <marcadecarlo@gmail.com>
2025-05-08 12:27:50 -0400

    vector

--- src/C/dsp_helper.c
+++ src/C/dsp_helper.c
@@ -8,10 +8,10 @@ inline void vectorize_q7_add_to_q31(
     size_t i = 0;
     // unroll 4 at a time
     for (; i + 3 < blockSize; i += 4) {
-        dst[i]     = ((int32_t)srcA[i]   << 24) + dst[i];
-        dst[i+1]   = ((int32_t)srcA[i+1]   << 24) + dst[i+1];
-        dst[i+2]   = ((int32_t)srcA[i+2]   << 24) + dst[i+2];
-        dst[i+3]   = ((int32_t)srcA[i+3]   << 24) + dst[i+3];
+        dst[i]     = ((int32_t)srcA[i]) + dst[i];
+        dst[i+1]   = ((int32_t)srcA[i+1]) + dst[i+1];
+        dst[i+2]   = ((int32_t)srcA[i+2]) + dst[i+2];
+        dst[i+3]   = ((int32_t)srcA[i+3]) + dst[i+3];
     }
     // leftover
     for (; i < blockSize; i++) {
